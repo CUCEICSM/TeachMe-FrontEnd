24ef414022a0e05288a6a0ee79357f12412bb08f
Cuauhtli Fernando <cuauh_fer_bh@hotmail.com>
2020-03-25 19:15:47 -0600

    Topics request

--- js/topic_request.js
+++ js/topic_request.js
@@ -1,5 +1,36 @@
-function openForm() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  
+function toggleList(e){
+  var header = e.children(":first");
+  var list = e.children(":last");
+  clearList();
+  if(list.css("display") != "flex"){
+
+    header.css("border-bottom", "5px solid rgba(15, 54, 187, 1)");
+    header.css("border-left", "50px solid rgba(15, 54, 187, 1)");
+    list.fadeIn(300);
+    list.css("display", "flex");
+  }
+}
+
+function clearList(){
+  $('.card h3').css("border-bottom", "5px solid #F0F0F0");
+  $('.card h3').css("border-left", "0px solid rgba(15, 54, 187, 1)");
+  $('.card ul').fadeOut(300);
+}
+
+function addVote(e){
+  var count = parseInt( e.find($('.solicitudes')).html() );
+  e.find($('.solicitudes')).html(count+1);
+  var genCount = parseInt( e.parent().parent().find($('h3 .solicitudes')).html());
+  e.parent().parent().find($('h3 .solicitudes')).html(genCount + 1);
+  e.find('.far').remove();
+  e.append('<i class="fas fa-thumbs-up" title="Eliminar solicitud" onclick="remVote($(this).parent());"></i>');
+}
+
+function remVote(e){
+  var count = parseInt( e.find($('.solicitudes')).html() );
+  e.find($('.solicitudes')).html(count-1);
+  var genCount = parseInt( e.parent().parent().find($('h3 .solicitudes')).html());
+  e.parent().parent().find($('h3 .solicitudes')).html(genCount - 1);
+  e.find('.fas').remove();
+  e.append('<i class="far fa-thumbs-up" title="Añadir solicitud" onclick="addVote($(this).parent());"></i>');
 }
